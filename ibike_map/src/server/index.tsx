4fc00878f5e4ae9edf283f2aec78caf1991758a0
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-12-13 21:08:49 +0800

    try static server

--- ibike_map/src/server/index.tsx
+++ ibike_map/src/server/index.tsx
@@ -1,1 +1,19 @@
-console.log('hello');
+import * as express from 'express';
+import * as path from 'path';
+
+const CLIENT_PATH = path.join(__dirname, '../client');
+
+export const createApp = () => {
+    const app = express();
+    app.use(express.static(CLIENT_PATH));
+
+    return app;
+}
+
+export const app = createApp();
+
+if (require.main === module) {
+    app.listen(4413, () => {
+        console.log('server is running.');
+    });
+}
--- ibike_map/src/server/webpack.config.js
+++ ibike_map/src/server/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         path: path.resolve(__dirname, '../..', 'dist/server')
     },
     target: 'node',
+    node: {
+        __dirname: false
+    },
     devtool: 'source-map',
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
